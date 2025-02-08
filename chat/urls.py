c5b8191678c33310f913c89e2c1d34cd1f08a92c
ibrohim <ibrohim.dev.uz@gmail.com>
2025-02-08 21:39:48 +0500

    .

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import SendMessageAPIView, UserMessagesAPIView
+from .views import SendMessageAPIView, UserMessagesAPIView, RegisterUserAPIView, LoginAPIView
 
 urlpatterns = [
     path("send-message/", SendMessageAPIView.as_view(), name="send-message"),
     path("messages/", UserMessagesAPIView.as_view(), name="user-messages"),
+
+    path("register/", RegisterUserAPIView.as_view(), name="register"),
+    path("login/", LoginAPIView.as_view(), name="login"),
+
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .models import Message
-from .serializers import MessageSerializer
+from .serializers import MessageSerializer, UserSerializer
 
 
 class SendMessageAPIView(APIView):
@@ -30,3 +33,26 @@ class UserMessagesAPIView(APIView):
         messages = messages.order_by("-timestamp")
         serializer = MessageSerializer(messages, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class RegisterUserAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "Foydalanuvchi muvaffaqiyatli yaratildi!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            })
+        return Response({"error": "Noto‘g‘ri username yoki parol!"}, status=status.HTTP_401_UNAUTHORIZED)
